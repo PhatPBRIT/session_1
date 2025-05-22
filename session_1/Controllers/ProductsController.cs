6622918785a3283ad689b85e9377ecd710e1dcd1
PhatBPRIT <hsthptbb.chphat11a10@gmail.com>
2025-05-22 18:50:36 +0700

    init project3

--- session_1/Controllers/ProductsController.cs
+++ session_1/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using session_1.Models;
 using session_1.Models.Interfaces;
 
 namespace session_1.Controllers
@@ -10,13 +9,23 @@ namespace session_1.Controllers
 
         public ProductsController(IProductRepository productRepository)
         {
-            _productRepository = productRepository;
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
         }
 
         public IActionResult Shop()
         {
             var products = _productRepository.GetAllProducts();
             return View(products);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var product = _productRepository.GetProductDetail(id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
         }
     }
 }
--- session_1/Program.cs
+++ session_1/Program.cs
@@ -1,14 +1,15 @@
-using session_1.Models.Services;
 using Microsoft.EntityFrameworkCore;
 using session_1.Data;
 using session_1.Models.Interfaces;
+using session_1.Models.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddDbContext<CoffeeShopDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeShopDbContextConnection")));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -16,6 +17,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -28,6 +30,14 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Products}/{action=Shop}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "product",
+    pattern: "{controller=Product}/{action=ListAll}/{id?}");
+
+app.MapControllerRoute(
+    name: "shop",
+    pattern: "{controller=Product}/{action=Shop}/{id?}");
 
 app.Run();
