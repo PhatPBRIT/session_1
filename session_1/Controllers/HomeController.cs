e3f4a3f3876f214718df8c593a50c7624e1bbae4
PhatBPRIT <hsthptbb.chphat11a10@gmail.com>
2025-05-27 09:34:13 +0700

    update_section_4

--- session_1/Controllers/HomeController.cs
+++ session_1/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace session_1.Controllers
             var product = new List<Product>
             {
                 new Product { Name = "Espresso", Price = 4.50m, ImageUrl = "https://file.hstatic.net/200000886877/article/ca-phe-espresso-1_f71dd8f8b25b4f92889956e66a636be6.jpg" },
-                new Product { Name = "Latte", Price = 3.50m, ImageUrl = "https://vinbarista.com/uploads/news/ca-phe-latte-la-gi-latte-co-vi-gi-latte-khac-gi-capuchino-202408161122.jpg" },
+                new Product { Name = "Latte", Price = 3.50m, ImageUrl = "https://vinbarista.com/uploads/news/ca-phe-latte-la-gi-latte-co-vi-gi-latte-khac-gi-capuchino-202408161122.jpg " },
                 new Product { Name = "Cappuccino", Price = 3.00m, ImageUrl = "https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-4.0.3&auto=format&fit=crop&w=300&q=80" }
             };
 
